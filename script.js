e31b633fd07caf6a21018bc2ad72eee360b4f6c1
Isabelaalvees <isabela.al8008@gmail.com>
2026-04-10 08:49:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ window.onload = function () {
   fetch('pais.html')
     .then(response => response.text())
     .then(html => {
-    document.getElementById('conteudo-pais').innerHTML = '<div class="cards">' + html + '</div>';
+    document.getElementById('conteudo-pais').innerHTML = html;
     });
     fetch('continente.html')
     .then(response => response.text())
